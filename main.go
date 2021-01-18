ac9399d1c31c04a1c9b2558293305a26c0eec3b7
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-18 09:28:58 +0900

    show create

--- main.go
+++ main.go
@@ -27,35 +27,8 @@ var boxInfo [9][9]int
 //itemInfo is item info
 var itemInfo [9][9][10]int
 
-//getBoxNum return item's box number
-func getBoxNum(ri, ci int) (index int) {
-	switch {
-	case ri >= 0 && ri <= 2 && ci >= 0 && ci <= 2:
-		index = 0
-	case ri >= 0 && ri <= 2 && ci >= 3 && ci <= 5:
-		index = 1
-	case ri >= 0 && ri <= 2 && ci >= 6 && ci <= 8:
-		index = 2
-	case ri >= 3 && ri <= 5 && ci >= 0 && ci <= 2:
-		index = 3
-	case ri >= 3 && ri <= 5 && ci >= 3 && ci <= 5:
-		index = 4
-	case ri >= 3 && ri <= 5 && ci >= 6 && ci <= 8:
-		index = 5
-	case ri >= 6 && ri <= 8 && ci >= 0 && ci <= 2:
-		index = 6
-	case ri >= 6 && ri <= 8 && ci >= 3 && ci <= 5:
-		index = 7
-	case ri >= 6 && ri <= 8 && ci >= 6 && ci <= 8:
-		index = 8
-	default:
-		fmt.Println("error")
-	}
-	return
-}
-
-//searchRow is searching same number in the Row
-func searchRow(ri, ci, item int) {
+//checkRow is searching same number in the Row
+func checkRow(ri, ci, item int) {
 	if item > 0 && item <= 9 {
 		for i := 0; i < 9; i++ {
 			itemInfo[ri][i][item] = 11
@@ -69,11 +42,11 @@ func searchRow(ri, ci, item int) {
 	}
 }
 
-//searchCol is searching same number in the col
-func searchCol(ri, ci, item int) {
+//checkCol is searching same number in the col
+func checkCol(ri, ci, item int) {
 	if item > 0 && item <= 9 {
 		for i := 0; i < 9; i++ {
-			itemInfo[ci][i][item] = 11
+			itemInfo[i][ci][item] = 11
 		}
 	} else if item == 10 {
 		for _, rowItem := range itemInfo[ci] {
@@ -84,10 +57,10 @@ func searchCol(ri, ci, item int) {
 	}
 }
 
-//searchBox is searching same number in the box
-func searchBox(ri, ci, item int) {
-	bIndex := getBoxNum(ri, ci)
-	startRi, endRi, startCi, endCi := getBox(bIndex)
+//checkBox is searching same number in the box
+func checkBox(ri, ci, item int) {
+
+	_, startRi, endRi, startCi, endCi := getBox(0, ri, ci)
 
 	if item >= 1 && item <= 9 {
 		for rIndex := startRi; rIndex <= endRi; rIndex++ {
@@ -105,25 +78,6 @@ func searchBox(ri, ci, item int) {
 		}
 	}
 }
-
-//searchInfoRow is searching same number in the itemInfo's row data
-/* func searchInfoRow(ri, ci, item int) {
-	var tmp [3]bool
-	//9 times search No.1 - No.9
-	for index := 1; index <= 9; index++ {
-		//3 times search
-		for j:=0;j<3;j++{
-			//row0 row1 ro2 cheack
-			for _,val:=range itemInfo[j]{
-				//
-				if val == index {
-					tmp[0] = true
-				}
-			}
-		}
-
-	}
-} */
 
 //getRow return row data
 func getRow() [9][9][10]int {
@@ -152,36 +106,36 @@ func getCol() [9][9][10]int {
 }
 
 //getBox return box data
-func getBox(index int) (startRi, endRi, startCi, endCi int) {
+func getBox(in, ri, ci int) (index, startRi, endRi, startCi, endCi int) {
 
-	switch index {
-	case 0:
+	switch {
+	case (ri >= 0 && ri <= 2 && ci >= 0 && ci <= 2) || in == 1:
+		index = 1
 		startRi, endRi, startCi, endCi = 0, 2, 0, 2
-		//startRi=0 , endRi=2 , startCi=0 , endCi=2
-	case 1:
+	case (ri >= 0 && ri <= 2 && ci >= 3 && ci <= 5) || in == 2:
+		index = 2
 		startRi, endRi, startCi, endCi = 0, 2, 3, 5
-		//startRi=0 , endRi=2 , startCi=3 , endCi=5
-	case 2:
+	case (ri >= 0 && ri <= 2 && ci >= 6 && ci <= 8) || in == 3:
+		index = 3
 		startRi, endRi, startCi, endCi = 0, 2, 6, 8
-		//startRi=0 , endRi=2 , startCi=6 , endCi=8
-	case 3:
+	case (ri >= 3 && ri <= 5 && ci >= 0 && ci <= 2) || in == 4:
+		index = 4
 		startRi, endRi, startCi, endCi = 3, 5, 0, 2
-		//startRi=3 , endRi=5 , startCi=0 , endCi=2
-	case 4:
+	case (ri >= 3 && ri <= 5 && ci >= 3 && ci <= 5) || in == 5:
+		index = 5
 		startRi, endRi, startCi, endCi = 3, 5, 3, 5
-		//startRi=3 , endRi=5 , startCi=3 , endCi=5
-	case 5:
+	case (ri >= 3 && ri <= 5 && ci >= 6 && ci <= 8) || in == 6:
+		index = 6
 		startRi, endRi, startCi, endCi = 3, 5, 6, 8
-		//startRi=3 , endRi=5 , startCi=6 , endCi=8
-	case 6:
+	case (ri >= 6 && ri <= 8 && ci >= 0 && ci <= 2) || in == 7:
+		index = 7
 		startRi, endRi, startCi, endCi = 6, 8, 0, 2
-		//startRi=6 , endRi=8 , startCi=0 , endCi=2
-	case 7:
+	case (ri >= 6 && ri <= 8 && ci >= 3 && ci <= 5) || in == 8:
+		index = 8
 		startRi, endRi, startCi, endCi = 6, 8, 3, 5
-		//startRi=6 , endRi=8 , startCi=3 , endCi=5
-	case 8:
+	case (ri >= 6 && ri <= 8 && ci >= 6 && ci <= 8) || in == 9:
+		index = 9
 		startRi, endRi, startCi, endCi = 6, 8, 6, 8
-		//startRi=6 , endRi=8 , startCi=6 , endCi=8
 	default:
 		fmt.Println("error")
 	}
@@ -190,30 +144,70 @@ func getBox(index int) (startRi, endRi, startCi, endCi int) {
 }
 
 //showInfo show Info. type is 'row or col or box or all'
-func showInfo(showType string) {
+func showInfo(showType string, sele int) {
 	switch showType {
 	case "row":
 		//row item print
-		for _, rowItem := range rowInfo {
-			fmt.Printf("%v\n", rowItem)
+		tmp := getRow()
+		for ri, _ := range tmp {
+			if sele == 100 {
+				for ci := 0; ci < 9; ci++ {
+					fmt.Printf("colNo: %v rowNo: %v item: %v\n", ri+1, ci+1, tmp[ri][ci])
+				}
+			} else if sele >= 0 && sele <= 10 {
+				var tmpRow [9]int
+				for ci := 0; ci < 9; ci++ {
+					tmpRow[ci] = tmp[ri][ci][sele]
+				}
+				fmt.Printf("rowNo: %v item: %v\n", ri+1, tmpRow)
+			}
 		}
 	case "col":
 		//col item print
-		/* for _, colItem := range colInfo {
-			fmt.Printf("%v\n", colItem)
-		} */
-		//all item print
 		tmp := getCol()
 		for ci, _ := range tmp {
-			for ri := 0; ri < 9; ri++ {
-				fmt.Printf("colNo: %v rowNo: %v item: %v\n", ci+1, ri+1, tmp[ci][ri])
+			if sele == 100 {
+				for ri := 0; ri < 9; ri++ {
+					fmt.Printf("colNo: %v rowNo: %v item: %v\n", ci+1, ri+1, tmp[ci][ri])
+				}
+			} else if sele >= 0 && sele <= 10 {
+				var tmpCol [9]int
+				for ri := 0; ri < 9; ri++ {
+					tmpCol[ri] = tmp[ci][ri][sele]
+				}
+				fmt.Printf("colNo: %v item: %v\n", ci+1, tmpCol)
 			}
 		}
 
 	case "box":
 		//box item print
+
 		for _, boxItem := range boxInfo {
 			fmt.Printf("%v\n", boxItem)
+		}
+
+		if sele == 100 {
+			for i := 1; i <= 9; i++ {
+				_, startRi, endRi, startCi, endCi := getBox(i, 100, 100)
+
+				for rIndex := startRi; rIndex <= endRi; rIndex++ {
+					for cIndex := startCi; cIndex <= endCi; cIndex++ {
+						fmt.Printf("showBox: index:%v value:%v \n", i, itemInfo[rIndex][cIndex])
+					}
+				}
+			}
+
+		} else if sele >= 0 && sele <= 10 {
+			for i := 1; i <= 9; i++ {
+				_, startRi, endRi, startCi, endCi := getBox(i, 100, 100)
+
+				for rIndex := startRi; rIndex <= endRi; rIndex++ {
+					for cIndex := startCi; cIndex <= endCi; cIndex++ {
+						fmt.Printf("showBox: index:%v namber:%v value:%v \n", i, sele, itemInfo[rIndex][cIndex][sele])
+					}
+				}
+			}
+
 		}
 	case "all":
 		//all item print
@@ -251,51 +245,38 @@ func main() {
 		}
 	}
 
-	//show
+	//show all
 	fmt.Println("show all-1:")
-	showInfo("all")
+	showInfo("all", 100)
 
 	//itemInfo check & set
 	for ri, rowItem := range itemInfo {
 		for ci, item := range rowItem {
+
 			//row check
-			searchRow(ri, ci, item[0])
+			checkRow(ri, ci, item[0])
 
 			//col check
-			searchCol(ri, ci, item[0])
+			checkCol(ri, ci, item[0])
 
 			//box check
-			searchBox(ri, ci, item[0])
+			checkBox(ri, ci, item[0])
 		}
 	}
 
-	//show
+	//show all
 	fmt.Println("show all-2:")
-	showInfo("all")
+	showInfo("all", 100)
 
-	/* //row item set
-	for ri, rowItem := range itemInfo {
-		for ci, item := range rowItem {
-			rowInfo[ri][ci] = item
-		}
-	}
-
-	//col item set
-	for ri, rowItem := range itemInfo {
-		for ci, item := range rowItem {
-			colInfo[ci][ri] = item
-		}
-	}
-
-	//box item set
-	for ri, rowItem := range itemInfo {
-		for ci, item := range rowItem {
-			if item > 0 {
-				boxInfo[getBoxNum(ri, ci)][item-1] = item
-			}
-		}
-	} */
-
+	//show col
 	fmt.Println("showCol: ")
-	showInfo("col")
+	showInfo("col", 1)
+
+	//show row
+	fmt.Println("showRow: ")
+	showInfo("row", 1)
+
+	//show box
+	fmt.Println("showBox: ")
+	showInfo("box", 1)
 }
